de7889c7a549619bfe82b71c158516b71e60c211
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-09-25 23:04:18 +0900

    [level 2] Title: 기능개발, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42586. 기능개발/기능개발.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(progresses, speeds):
+    answer = []
+    q_pro = deque(progresses)
+    q_spd = deque(speeds)
+    i = 0
+    while q_pro:
+        i += 1
+        a = 0
+        while q_pro and q_pro[0] + q_spd[0] * i >= 100:
+            q_pro.popleft()
+            q_spd.popleft()
+            a += 1
+        if a: answer.append(a)
+    return answer
