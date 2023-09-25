fca13028f5dd84f0fa16910a406893dc0bfee9ce
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-09-25 23:08:23 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -0,0 +1,9 @@
+def solution(n, lost, reserve):
+    _lost = sorted([l for l in lost if l not in reserve])
+    _reserve = sorted([r for r in reserve if r not in lost])
+    for r in _reserve:
+        if r - 1 in _lost:
+            _lost.remove(r - 1)
+        elif r + 1 in _lost:
+            _lost.remove(r + 1)
+    return n - len(_lost)
