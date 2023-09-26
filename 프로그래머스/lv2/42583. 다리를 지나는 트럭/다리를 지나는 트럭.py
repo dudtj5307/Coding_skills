28263f55a8fbfeb8ea72af4570c95e3f950d8202
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-09-26 23:45:58 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+def solution(bridge_length, weight, truck_weights):
+    s_q, e_q, m_q = deque(truck_weights), deque(), deque([0 for _ in range(bridge_length)])
+    m = 0
+    answer = 0
+    while len(e_q) != len(truck_weights):
+        tempm = m_q.popleft()
+        if tempm: 
+            e_q.append(tempm)
+            m -= tempm
+        if s_q and m + s_q[0] <= weight:
+            temps = s_q.popleft()
+            m_q.append(temps)
+            m += temps
+        else:
+            m_q.append(0)
+        answer += 1
+    return answer 
