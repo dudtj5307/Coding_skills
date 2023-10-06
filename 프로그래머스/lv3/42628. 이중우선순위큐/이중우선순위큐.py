72806c35a929c66209d78f9abf8e32a7fa5b6612
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-10-06 21:25:33 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -0,0 +1,19 @@
+from heapq import heappush, heappop, nlargest, heapify
+
+def solution(operations):
+    hq = []
+    for o in operations:
+        order, n = o.split(" ")
+        if order == "I":
+            heappush(hq, int(n))
+        elif hq and order == "D":
+            if int(n) == 1:
+                largnum = nlargest(1, hq)[0]
+                hq.remove(largnum)
+                # hq = heapify(hq)
+            else: heappop(hq)
+        print(o, hq)
+    if hq:
+        return [max(hq), min(hq)]
+    else:
+        return [0, 0]
