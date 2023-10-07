3b142995e0f2f7f7525050a9b7183e48fa77adc3
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-10-08 01:28:10 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -2,9 +2,7 @@ def solution(s):
     rev = list(reversed(s.split(" ")))
     i, answer = 0, 0
     while i < len(rev):
-        if rev[i] == 'Z':
-            i += 1
-        else:
-            answer += int(rev[i])
+        if rev[i] == 'Z': i += 1
+        else: answer += int(rev[i])
         i += 1
     return answer
