e5606d004f32064be2ac4e02ff2dc3ea0f9f0674
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-12-03 23:19:16 +0900

    [level 3] Title: 가장 먼 노드, Time: 22.71 ms, Memory: 19.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -0,0 +1,18 @@
+from collections import deque
+
+def solution(n, edge):
+    node = [[] for _ in range(n+1)]
+    visit = [0 for i in range(n+1)]
+    for a, b in edge:
+        node[a].append(b)
+        node[b].append(a)
+    
+    visit[1] = 1
+    q = deque([1])
+    while q:
+        k = q.popleft()
+        for n in node[k]:
+            if not visit[n]:
+                visit[n] = visit[k] + 1
+                q.append(n)
+    return visit.count(max(visit))
