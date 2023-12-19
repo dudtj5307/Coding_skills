422bfb750a181e714c53dba2b190d1b6ca275bb7
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-12-19 23:25:25 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -2,12 +2,11 @@ def solution(skill, skill_trees):
     answer = 0
     for sk in skill_trees:
         seq = skill[:]
-        flag = 1
         for s in sk:
             if s in seq:
                 if s == seq[0]:
                     seq = seq[1:]
                 else:
-                    flag = 0
-        if flag: answer += 1
+                    break
+        else: answer += 1
     return answer
