9559d1aad242326ddd3be1b40aa33fa191ed406b
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2023-12-26 23:52:35 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 def solution(fees, records):
     def_time, def_fee, u_time, u_fee = fees
     dic = {}
-    usage = defaultdict(int)
+    answer = defaultdict(int)
     for record in records:
         t, car, status = record.split()
         h, m = map(int, t.split(':'))
@@ -13,14 +13,8 @@ def solution(fees, records):
         else:
             in_time = dic[car]
             del dic[car]
-            usage[car] += time - in_time
-    end_time = 23 * 60 + 59
-    for car, in_time in dic.items():
-        usage[car] += end_time - in_time
-    answer = []
-    for car, use_time in sorted(usage.items(), key=lambda x:x[0]):
-        if use_time <= def_time:
-            answer.append(def_fee)
-        else:
-            answer.append(def_fee + -(-(use_time - def_time) // u_time) * u_fee)
-    return answer
+            if time - in_time <= def_time:
+                answer[car] += def_fee
+            else:
+                answer[car] += def_fee + -(-(time - in_time - def_time) // u_time) * u_fee
+                print(answer)
