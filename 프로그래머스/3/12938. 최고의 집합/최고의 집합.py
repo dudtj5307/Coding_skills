77fafe092b3751d189c7564ee40cfd2cc545618c
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2024-01-02 00:38:41 +0900

    [level 3] Title: 최고의 집합, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
+++ 프로그래머스/3/12938. 최고의 집합/최고의 집합.py
@@ -1,10 +1,4 @@
 def solution(n, s):
-    if s < 3 * n:
-        return [-1]
-    big = ((s // n) + 1 * bool(s % n)) * n
-    diff = big - s
-    each = big // n
-    answer = [each for _ in range(n)]
-    for i in range(diff):
-        answer[i] -= 1
-    return answer
+    div, mod = divmod(s, n)
+    if div == 0: return [-1]
+    return [div] * (n - mod) + [div+1] * mod
