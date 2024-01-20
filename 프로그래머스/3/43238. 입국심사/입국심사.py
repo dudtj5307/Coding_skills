bf0e9873485de1e690ff9efe0625663e72345b4e
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2024-01-21 00:33:45 +0900

    [level 3] Title: 입국심사, Time: 0.05 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,17 @@
+def solution(n, times):
+    left, right = 1, n * max(times)
+    while left <= right:
+        mid = (left + right) // 2
+        people = 0
+        for t in times:
+            people += mid // t
+            if people >= n: break
+        if people >= n:
+            right = mid - 1
+            answer = mid
+        else:
+            left = mid + 1
+        # print(mid, left, right)
+    return answer
+
+            
