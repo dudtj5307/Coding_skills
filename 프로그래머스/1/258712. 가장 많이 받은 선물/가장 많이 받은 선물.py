44e358ff44ba0cfb304bae2a47022e00e026e725
Young Suh Lee <74390376+dudtj5307@users.noreply.github.com>
2024-01-23 23:54:35 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 2.34 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+from itertools import combinations
+
+def solution(friends, gifts):
+    give, take = defaultdict(int), defaultdict(int)
+    g_dic = defaultdict(lambda: defaultdict(int))
+    for g, t in map(lambda x:x.split(" "), gifts):
+        give[g] += 1
+        take[t] += 1
+        g_dic[g][t] += 1
+    answer = defaultdict(int)
+    for a, b in combinations(friends, 2):
+        if g_dic[a][b] > g_dic[b][a]:
+            answer[a] += 1
+        elif g_dic[a][b] < g_dic[b][a]:
+            answer[b] += 1
+        else:
+            if give[a] - take[a] > give[b] - take[b]:
+                answer[a] += 1
+            elif give[a] - take[a] < give[b] - take[b]:
+                answer[b] += 1
+            else:
+                answer[a] += 0
+    return max(answer.values())
+    
+    
